0b8b7ceb0b4ff40a2b8e4dfdb3e31415e41c1271
Fabio Yamashita <yamashitafabio@gmail.com>
2022-06-25 12:27:28 -0300

    Adicionando class Piano

---
+++ ConsoleApp2/Piano.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula24062022
+{
+    internal class Piano : Instrument
+    {
+        public int NumKey { get; set; }
+        public string Type { get; set; } // Cauda, de armário, digital
+    }
+}
