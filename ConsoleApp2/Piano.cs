57cc93a5238564d9eb605968af7ff42e6d60b973
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-06-27 15:08:50 -0300

    feat: adding new piano class

---
+++ ConsoleApp2/Piano.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Piano : Instrument
+    {
+        public int KeysCount { get; set; }
+        public bool IsAcoustic { get; set; }
+        public bool HasPedal { get; set; }
+    }
+}
